10ac839a87e09e84fd5b736a06649a3ca3a86009
electromecaniconicolas-dot <electromecaniconicolas@gmail.com>
2025-12-01 17:27:33 -0500

    eentra de formulario de agregar habitacion mediante login de admin

--- scripts/login.js
+++ scripts/login.js
@@ -11,6 +11,26 @@ document.getElementById("btnLogin").addEventListener("click", function () {
         return;
     }
 
+    // --- VALIDACIÓN PARA EL ADMIN ---
+    if (email === "AdminHotel@gmail.com" && password === "2123holiwis") {
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Bienvenido Administrador',
+            text: 'Redirigiendo...',
+            timer: 1500,
+            showConfirmButton: false
+        });
+
+        setTimeout(() => {
+            window.location.href = "../reservas/crearHabitacion.html"; 
+            // Ajusta esta ruta al archivo real
+        }, 1500);
+
+        return;
+    }
+
+    // --- SI NO ES ADMIN: Login normal ---
     Swal.fire({
         icon: 'success',
         title: 'Bienvenido',
